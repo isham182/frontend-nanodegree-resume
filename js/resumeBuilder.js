7d4a1e3481374c3635c433168629cc02d4ffc010
isham182 <isham182@yahoo.com>
2015-07-24 21:22:14 -0500

    Added name, and Role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,15 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+$("#main").append("Josh Isham");
+var awesomethoughts = ("I am Josh and I am Awesome!");
+console.log (awesomethoughts);
+var funthoughts = 
+	awesomethoughts.replace("Awesome", "Fun");
+console.log (funthoughts)
+
+var name = "Josh Isham"
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Front-end Web Developer"
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
